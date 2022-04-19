0a96c5271bbbec680b8d9e052a5c419e87a19a0e
unknown <sreekanta.csd@gmail.com>
2022-04-19 07:14:24 +0600

    button added

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
+
+    const addToCart = (id) =>{
+        console.log("button added", id)
+    }
+    const addToCartWithParam =() =>addToCart(id)
     return (
-        <div>
+        <div className='product'>
             <h2>product id:{id}</h2>
             <h3>Buy this item for your body care:{name}</h3>
             <p>Selling price:${price}</p>
+            <button onClick={addToCartWithParam}>Add to Cart</button>
         </div>
     );
 };
